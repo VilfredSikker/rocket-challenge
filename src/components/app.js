29d9281a1f722bbd9e39111fae20a9aa328d3c20
Vilfred Sikker <vilfredsikker@gmail.com>
2021-09-19 21:57:33 +0200

    upgrade packages

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes, Route } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 import { Flex, Text } from "@chakra-ui/core"
 
 import Launches from "./launches"
@@ -15,13 +15,13 @@ export default function App() {
     <div>
       <NavBar />
       <FavoritesDrawer />
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <Switch>
+        <Route exact path="/" element={<Home />} />
         <Route path="/launches" element={<Launches />} />
         <Route path="/launches/:launchId" element={<Launch />} />
         <Route path="/launch-pads" element={<LaunchPads />} />
         <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
-      </Routes>
+      </Switch>
     </div>
   )
 }
